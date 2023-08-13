8df612cfeb9d6bebca77914b0a0ab54f9ee0fc8e
SaoYan <SaoYan@users.noreply.github.com>
2023-08-13 21:34:15 +0000

    Code generated by codemakerai

--- example.java
+++ example.java
@@ -15,7 +15,18 @@ public class PaymentDao {
    * @throws IllegalArgumentException if payment is null
    */
   public void save(Payment payment) {
-  }
+        if (payment == null) {
+            throw new IllegalArgumentException("Payment cannot be null");
+        }
+        try {
+            session.beginTransaction();
+            session.save(payment);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            logger.error("Error while persisting payment", e);
+            session.getTransaction().rollback();
+        }
+    }
   
   /**
    * Updates the payment in the database.
@@ -23,7 +34,15 @@ public class PaymentDao {
    * @param payment payment to save
    */
   public void update(Payment payment) {
-  }
+        try {
+            session.beginTransaction();
+            session.saveOrUpdate(payment);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            logger.error("Error while updating payment: {}", e.getMessage());
+            session.getTransaction().rollback();
+        }
+    }
   
   /**
    * Retrieves the payment from the database.
@@ -31,7 +50,16 @@ public class PaymentDao {
    * @return payment the payment or null
    */
   public Payment get(String id) {
-  }
+        Payment payment = null;
+        try {
+            Query query = session.createQuery("from Payment where id = :id");
+            query.setParameter("id", id);
+            payment = (Payment) query.uniqueResult();
+        } catch (HibernateException e) {
+            logger.error("Error retrieving payment with id: " + id, e);
+        }
+        return payment;
+    }
   
   /**
    * Deletes the payment from the database.
@@ -39,5 +67,13 @@ public class PaymentDao {
    * @param payment payment to save
    */
   public void delete(Payment payment) {
-  }
+        try {
+            session.beginTransaction();
+            session.delete(payment);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            logger.error("Error deleting payment: " + e.getMessage());
+            session.getTransaction().rollback();
+        }
+    }
 }
