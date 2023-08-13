986f55d3663f4c7579c7d3acf30705e679e88b71
SaoYan <25550286+SaoYan@users.noreply.github.com>
2023-08-13 13:46:28 -0700

    Install wget uzip

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ api_key=$1
 mode=$2
 path=$3
 
+apt-get install wget uzip
 wget https://github.com/codemakerai/codemaker-cli/releases/download/v0.0.12/linux-amd64.zip -P /usr/local/bin/codemaker-cli
 uzip /usr/local/bin/codemaker-cli/linux-amd64.zip
 export PATH=$PATH:/usr/local/bin/codemaker-cli/linux-amd64/bin
